33189177b8a6c2e9fb0eab5a287731351693fea3
Sylvain CHAU <sylvain.chau@ensiie.eu>
2025-03-21 21:16:11 +0100

    B.1 main.c Libération de la mémoire

--- src/main.c
+++ src/main.c
@@ -5,35 +5,42 @@
 #include "../include/player.h"
 #include "../include/interface.h"
 
-#define NUM_TEAMS 2
+#define NB_TEAMS 2
 #define NB_PLAYERS_TEAM 2
 #define NB_ROUNDS 3
 
 int main() {
-    // Initialisation du plateau
+
+    /* Initialisation du plateau */
+
     board b = create_board();
     
-    // Ajout des équipes et des joueuses
-    for (int i = 0; i < NUM_TEAMS; i++) {
+    /* Ajout des équipes et des joueuses */
+
+    for (int i = 0; i < NB_TEAMS; i++) {
         add_team(b);
         for (int j = 0; j < NB_PLAYERS_TEAM; j++) {
             player p = create_player();
             add_player_to_team(b, i, p);
-            // Distribution des cartes (exemple avec 5 cartes par joueuse)
+
+            /* Distribution des cartes */
+
             for (int k = 0; k < 5; k++) {
                 card c = create_card();
-                set_value(c, (k + 1)); // Exemple de distribution
+                set_value(c, (k + 1)); 
                 add_card_to_hand(p, c);
             }
         }
     }
     
-    // Déroulement des tours
+    /* Déroulement des tours */
+
     for (int round = 0; round < NB_ROUNDS; round++) {
         display_board(b);
         
-        // Phase de pari
-        for (int i = 0; i < NUM_TEAMS; i++) {
+        /* Phase de pari */
+
+        for (int i = 0; i < NB_TEAMS; i++) {
             for (int j = 0; j < NB_PLAYERS_TEAM; j++) {
                 player p = get_player(b, i, j);
                 int gamble = ask_gamble(p);
@@ -41,31 +48,41 @@ int main() {
             }
         }
         
-        // Phase de jeu
-        int team_scores[NUM_TEAMS] = {0, 0};
-        for (int i = 0; i < NUM_TEAMS; i++) {
+        /* Phase de jeu */
+
+        int team_scores[NB_TEAMS] = {0, 0};
+        for (int i = 0; i < NB_TEAMS; i++) {
             for (int j = 0; j < NB_PLAYERS_TEAM; j++) {
                 player p = get_player(b, i, j);
-                int num_cards = ask_number_of_played_cards(p);
-                for (int k = 0; k < num_cards; k++) {
+                int nb_cards = ask_number_of_played_cards(p);
+                for (int k = 0; k < nb_cards; k++) {
                     card c = ask_card(p);
+                    remove_card_from_hand(p,c);
                     play_card(p, c);
                     team_scores[i] += get_value(c);
                 }
             }
         }
         
-        // Détermination du gagnant du tour
-        int max_score = 0, winning_team = -1;
-        for (int i = 0; i < NUM_TEAMS; i++) {
+        /* Détermination du gagnant du tour */
+
+        int max_score = 0;
+        int winning_team = 1;
+
+        if(team_scores[0] == team_scores[1]){
+            display_message("Il y a égalité");
+        }
+
+        for (int i = 0; i < NB_TEAMS; i++) {
             if (team_scores[i] > max_score) {
                 max_score = team_scores[i];
                 winning_team = i;
             }
         }
         
-        // Attribution des points
-        for (int i = 0; i < NUM_TEAMS; i++) {
+        /* Attribution des points */
+
+        for (int i = 0; i < NB_TEAMS; i++) {
             for (int j = 0; j < NB_PLAYERS_TEAM; j++) {
                 player p = get_player(b, i, j);
                 int gamble = get_slate(p);
@@ -78,10 +95,19 @@ int main() {
         }
     }
     
-    // Fin du jeu
+    /* Fin du jeu */
     display_end_game(b);
     
-    // Libération de la mémoire
+    /* Libération de la mémoire  */
+    for (int i = 0; i < NB_TEAMS; i++) {
+        for (int j = 0; j < NB_PLAYERS_TEAM; j++) {
+            player p = get_player(b, i, j);
+
+            free_player(p);
+        }
+    }
+
     free_board(b);
+
     return 0;
 }
