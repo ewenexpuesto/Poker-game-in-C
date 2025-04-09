b116ff87dea2548894d178ec35549fcfa0ebdfa1
ewenexpuesto <ewen.expuesto@gmail.com>
2025-04-09 19:15:21 +0200

    Tâche D.1 completed probabiliste.c|h with basic functionalities

--- research_23/experiments/probabiliste.h
+++ research_23/experiments/probabiliste.h
@@ -12,7 +12,7 @@
 /**
  * \brief Cette fonction joue au jeu selon deux méthodes : la méthode agressive pour une équipe, et la méthode probabiliste pour l'autre équipe
  * \param 
- * \return une liste contenant en premier le nombre de points de l'équipe qui utilise la méthode agressive, et en deuxième le nombre de points obtenus par l'équipe utilsiant la méthode probabiliste
+ * \return une liste contenant en premier le score de l'équipe qui utilise la méthode agressive, et en deuxième le score obtenu par l'équipe utilsiant la méthode probabiliste
  */
 int * probabiliste();
 
