562bea07c2474d053e1288b9cf7a57904555dc71
Lilou MALFOY <lilou.malfoy@ensiie.eu>
2025-04-11 18:10:26 +0000

    Edit exp1.c

--- research_23/experiments/exp1.c
+++ research_23/experiments/exp1.c
@@ -12,7 +12,9 @@
 
 int main()
 {
+    printf("ca commence");
     FILE *fd = fopen("resultats1.csv", "w");
+    
     if (!fd)
     {
         perror("Erreur ouverture fichier resultats.csv");
@@ -25,8 +27,10 @@ int main()
         srand(time(NULL));
         /*la partie se joue en n tours avec n compris entre 3 et 20*/
         int n = (rand() % 18) + 3;
+        printf("n :%d", n);
         clock_t debut = clock();
         int scores = probabiliste(n); /*on lance une partie avec la fonction probabiliste*/
+        printf("scores : %d", scores);
         clock_t fin = clock();
         double temps_ms = ((double)(fin - debut)) * 1000.0 / CLOCKS_PER_SEC;
         int victoires = scores[1];
