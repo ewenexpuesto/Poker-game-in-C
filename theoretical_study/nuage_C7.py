020b2c0e1add344edba8c1cdb121b5df8fa53277
Lilou MALFOY <lilou.malfoy@ensiie.eu>
2025-04-24 11:05:34 +0000

    Upload New File

---
+++ theoretical_study/nuage_C7.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+#lecture du fichier
+df3 = pd.read_csv("data3.csv")
+
+#creation du nuage de points
+plt.scatter(df3['n'], df3['proba'], color='green', label='nuage de points (n, P_approx(2,3,1,2,n)')
+
+
+#lecture du fichier
+df4 = pd.read_csv("data4.csv")
+#creation de la doite
+plt.plot(df['N'], df['y'], color='red', label=f'y = P( (C^2_1,C^2_2) = (2,3) | (C^1_1,C^1_2)=(1,2) ))')
+
+#ajout des titres
+plt.xlabel("N")
+plt.ylabel("P_approx(2,3,1,2,N)")
+plt.title ("Nuage de points")
+plt.show()
