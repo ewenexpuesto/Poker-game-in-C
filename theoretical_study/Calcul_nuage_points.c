807f0103ee14335d82f03f0b728292e3c11dc20e
Marie PREZIOSO <marie.prezioso@ensiie.eu>
2025-04-24 09:50:04 +0000

    code nuage points tache c4 lot c

--- theoretical_study/Calcul_nuage_points.c
+++ theoretical_study/Calcul_nuage_points.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "tache_c3.h"
 
 void nuage_points_C4()
 {
@@ -7,14 +8,15 @@ void nuage_points_C4()
     if(f == NULL) // vérifie que le fichier s'eest bien ouvert
     {
         perror("Erreur lors de l'ouverture du fichier");
-        return 1;
+  
     }
     fprintf(f,"n,P_approx_1(i,j,n)\n"); // Ligne pour les titres des colonnes
     for(int n=10; n<100000; n++)
     {
-        fprintf(f, "%d,%d\n",n,P_approx_1(1,2,n));
+        fprintf(f, "%d,%f\n",n,P_approx_1_main(1,2,n));
     }
     fclose(f);
+   
 }
 
 void droite_C4()
@@ -23,9 +25,11 @@ void droite_C4()
     if(f == NULL) // vérifie que le fichier s'eest bien ouvert
     {
         perror("Erreur lors de l'ouverture du fichier");
-        return 1;
+        
     }
+    tirage t = {1,2};
     fprintf(f,"y\n"); // Ligne pour les titres de la colonne
-    fprintf(f, "%d\n",P(1,2));
+    fprintf(f, "%f\n",P(t));
     fclose(f);
+   
 }
