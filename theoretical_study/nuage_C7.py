7430f0bc6ce0826045c2bdd7b244de7b1421bd5a
Lilou MALFOY <lilou.malfoy@ensiie.eu>
2025-04-24 11:06:45 +0000

    Edit nuage_C7.py

--- theoretical_study/nuage_C7.py
+++ theoretical_study/nuage_C7.py
@@ -12,7 +12,7 @@ plt.scatter(df3['n'], df3['proba'], color='green', label='nuage de points (n, P_
 #lecture du fichier
 df4 = pd.read_csv("data4.csv")
 #creation de la doite
-plt.plot(df['N'], df['y'], color='red', label=f'y = P( (C^2_1,C^2_2) = (2,3) | (C^1_1,C^1_2)=(1,2) ))')
+plt.plot(df4['N'], df4['y'], color='red', label=f'y = P( (C^2_1,C^2_2) = (2,3) | (C^1_1,C^1_2)=(1,2) ))')
 
 #ajout des titres
 plt.xlabel("N")
