75c057e90fb0c067c8c0f3fa8111700c756160bb
Marie PREZIOSO <marie.prezioso@ensiie.eu>
2025-04-24 09:47:06 +0000

    Tache C6 lot c

--- theoretical_study/tache_c6.c
+++ theoretical_study/tache_c6.c
@@ -2,15 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 
+srand(time(NULL)); // initialisation du générateur de nombres aléatoires
+
 /** 
 \brief :  simule la distribution des cartes à J2 en prenant en argument la main (i,j) de J1
 \param : i et j entiers, cartes de J1
-\return : rien
+\return : tableau avec les 2 cartes distribuées à J2
 */
 
 int* main_J2(int i, int j)
 {
-    int cartes[4]; // préparation d'un tableau pour stocker les cartes qu'il rest dans le jeu
+    int cartes[4]; // préparation d'un tableau pour stocker les cartes qu'il reste dans le jeu
     int n1 = 2; // initialisation à 2 des entiers qui représentent le nombre de chaque cartes restantes
     int n2 = 2;
     int n3 = 2;
@@ -44,31 +46,33 @@ int* main_J2(int i, int j)
         exit(1);
     }
     int index = 0;
-    for (int k1=0; k1<n1; k1++)
+    for (int k1=0; k1<n1; k1++) // boucle pour mettre les potentielles cartes 1
     {
         cartes[index++]= 1;
     }
-    for (int k2=0; k2<n2; k2++)
+    for (int k2=0; k2<n2; k2++) // boucle pour mettre les potentielles cartes 2
     {
         cartes[index++]= 2;
     }
-    for (int k3=0; k3<n3; k3++)
+    for (int k3=0; k3<n3; k3++) // boucle pour mettre les potentielles cartes 3
     {
         cartes[index++]= 3;
     }
-    int* tab_main_J2 = malloc(2 * sizeof(int));
+    int* tab_main_J2 = malloc(2 * sizeof(int)); // tableau pour mettre les cartes de J2
     if (tab_main_J2 == NULL) 
     {
         perror("Échec malloc");
         return NULL;
     }
-    srand(time(NULL));
-    for (int m = 0; m<2; m++)
+    int index1 = rand() % 4; // on veut un index entre 1 et 4
+    int index2;
+    do // bouble do while qui tire 2 indexs différents, en recommençant si il a tiré 2 fois le même
     {
-        int index2 = rand() % 4;
-        tab_main_J2[m] = cartes[index2];
-    }
-    if(tab_main_J2[0] > tab_main_J2[1]) // ou les valeurs ne son pas rangées dans l'ordre croissant
+    index2 = rand() % 4;
+    } while (index2 == index1);
+    tab_main_J2[0] = cartes[index1];
+    tab_main_J2[1] = cartes[index2];
+    if(tab_main_J2[0] > tab_main_J2[1]) // cas ou les valeurs ne son pas rangées dans l'ordre croissant
     {
         int tmp = tab_main_J2[0];
         tab_main_J2[0] = tab_main_J2[1];
@@ -79,39 +83,40 @@ int* main_J2(int i, int j)
 
 /** 
 \brief : retourne a proportion de mains (i',j') obtenues par J2 lors de n distributions indépendantes de la main de J2, conditionnellement à (C^1_1,C^1_2)=(i,j)
-\param : 
-\return : 
+\param : i, j cartes de J1 et i', j' les cartes potentiellements obtenues par J2, n nombre de distributions
+\return : proportion
 */
-int P_approx_2_1_main(int i_prim, int j_prim, int i, int j, int n)
+double P_approx_2_1_main(int i_prim, int j_prim, int i, int j, int n)
 {
-    int p = 0;
-    for ( int k =0; k<n; k++)
+    int p = 0; // initialisation de la variable qui va compter les tirages (i', j')
+    for ( int k =0; k<n; k++) // boucle pour faire n tirages
     {
-        int* t_main_J2 = main_J2(i, j);
+        int* t_main_J2 = main_J2(i, j); // main de J2
         if (t_main_J2[0]==i_prim && t_main_J2[1]==j_prim)
         {
             p++;
         }
+        free(t_main_J2);
     }
-    int proportion = p/n ;
+    double proportion = (double)p/n ;
     return proportion;
 }
 
 /** 
 \brief : retourne pour chaque main (i',j'), la proportion de telles mains obtenues par J2 lors de n distributions indépendantes de la main de J2, conditionnellement à (C^1_1,C^1_2)=(i,j).
-\param: 
-\return : 
+\param: int i 
+\return : tableau avec la proportion pour chaque main possible de J2
 */
-int* P_approx_2_1(int i, int j ,int n)
+double* P_approx_2_1(int i, int j ,int n)
 {
-    int* tab = malloc(6*sizeof(int)); // il y a 6 couples de cartes 
-    int index=0;
-    for(int k=1; k<4; k++)
+    double* tab = malloc(6*sizeof(double)); // il y a 6 couples de cartes 
+    int index=0; // initialisation de l'index du tableau
+    for(int k=1; k<4; k++) // double boucle pour les mains (i', j')
     {
         for (int m =k; m<4; m++)
         {
-            int p = P_approx_2_1_main(k, m, i, j, n);
-            printf("La proportion de (%d,%d) est %d", i, j ,p);
+            double p = P_approx_2_1_main(k, m, i, j, n);
+            printf("La proportion de (%d,%d) est %f", k, m ,p);
             tab[index] = p;
             index++;
 
