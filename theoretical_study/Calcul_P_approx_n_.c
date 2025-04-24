994fac3f61c9023173e20887a67403dd5f88bcdc
Marie PREZIOSO <marie.prezioso@ensiie.eu>
2025-04-24 09:49:21 +0000

    code histogramme tache c4 lot c

--- theoretical_study/Calcul_P_approx_n_.c
+++ theoretical_study/Calcul_P_approx_n_.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include " probability_of_hand_empiric.h"
+#include "tache_c3.h"
 
 void hist_loi_empirique_C4(int n)
 {
     FILE *f = fopen("P_approx_1.csv","w"); // ouverture/créée fichier en mode écriture
-    if(f == NULL) // vérifie que le fichier s'eest bien ouvert
+    if(f == NULL) // vérifie que le fichier s'est bien ouvert
     {
         perror("Erreur lors de l'ouverture du fichier");
-        return 1;
     }
     fprintf(f," C¹_1,C¹_2,P_approx_1(n)\n"); // Ligne pour les titres des colonnes
-    int *tab = P_approx_1(n); //stock les valeurs de P_approx_1(n)
-    int i =0;
-    while(tab[i][2]!=NULL)
+    double *tab = P_approx_1(n); //stock les valeurs de P_approx_1(n)
+    int k =0;
+    while(tab[k]<6)
     {
-        fprintf(f,"%d,%d,%d",C¹_1,C¹_2, tab[i][2] );
-        i++;
+        for (int i=0; i<4; i++)
+        {
+            for(int j=i; j<4; j++)
+            {
+                fprintf(f,"%d,%d,%f",i,j, tab[k] );
+                k++;
+            }
+        }
     }
     fclose(f);
 }
@@ -27,22 +32,15 @@ void hist_loi_theorique_C4()
     if(f == NULL) // vérifie que le fichier s'eest bien ouvert
     {
         perror("Erreur lors de l'ouverture du fichier");
-        return 1;
     }
     fprintf(f," C¹_1,C¹_2,P(C¹_1,C¹_2)\n"); // Ligne pour les titres des colonnes
     for(int j=1; j<4; j++)
     {
         for (int i=1; i<=j; i++)
         {
-            fprintf(f, "%d,%d,%d\n", i,j,P(i,j));
+            tirage t = {i,j};
+            fprintf(f, "%d,%d,%f\n", i,j,P(t));
         }
     }
     fclose(f);
-}
-
-int main()
-{
-    int n = 1000;
-    hist_loi_empirique_C4(n);
-    hist_loi_theorique_C4();
 }
