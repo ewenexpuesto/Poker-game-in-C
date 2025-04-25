0197939ade17806ddf281c033582f3eae4de62f6
Ewen EXPUESTO <ewen.expuesto@ensiie.eu>
2025-04-25 15:05:14 +0000

    Tâche C.3 small mistakes

--- theoretical_study/tache_c3.h
+++ theoretical_study/tache_c3.h
@@ -1,11 +1,6 @@
 #ifndef PROBABILITY_OF_HAND_EMPIRIC_H
 #define PROBABILITY_OF_HAND_EMPIRIC_H
 
-typedef struct {
-    int first;
-    int second;
-} tirage;
-
 tirage distrib(int * pioche);
 
 tirage main_J1 ();
