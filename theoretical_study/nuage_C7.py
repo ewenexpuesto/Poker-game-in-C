885d2e21d583841e746762bb90a4d3eae1eab898
Lilou MALFOY <lilou.malfoy@ensiie.eu>
2025-04-25 13:47:11 +0000

    Edit nuage_C7.py

--- theoretical_study/nuage_C7.py
+++ theoretical_study/nuage_C7.py
@@ -10,7 +10,7 @@ valeur_theorique = df4.iloc[0, 0]  #On extrait la valeur de df4
 
 #Nuage de points :
 plt.figure(figsize=(10, 6))
-plt.scatter(df3['N'], df3['proba'], color='green', s=10, label='P_approx_2_1_main(2,3,1,2,n)')
+plt.scatter(df3['N'], df3['proba'], color='pink', s=10, label='P_approx_2_1_main(2,3,1,2,n)')
 
 # Tracer la droite horizontale
 plt.axhline(y=valeur_theorique, color='red', linestyle='--', label=f"y = P((2,3)|(1,2)) ≈ {valeur_theorique:.4f}")
