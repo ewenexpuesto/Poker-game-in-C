7a8d583ad0a09babfb016d6623b1622eead6d29d
Sylvain CHAU <sylvain.chau@ensiie.eu>
2025-04-25 19:03:32 +0200

    main lot_c

--- theoretical_study/main.c
+++ theoretical_study/main.c
@@ -10,16 +10,16 @@ void hist_loi_empirique_C9(int n)
     {
         perror("Erreur lors de l'ouverture du fichier");
     }
-    fprintf(f," C^2_1,C^2_2,P_approx_2(n)\n"); // Ligne pour les titres des colonnes
-    double *tab = P_approx_2(n); //stock les valeurs de P_approx_2(n)
+    fprintf(f,"(C2_1.C2_2),P_approx_2(n)\n"); // Ligne pour les titres des colonnes
+    double *tab = P_approx_2(n); //stock les valeurs de P_approx_1(n)
     int k =0;
-    while(tab[k]<6)
+    while(k<6)
     {
-        for (int i=0; i<4; i++)
+        for (int i=1; i<4; i++)
         {
             for(int j=i; j<4; j++)
             {
-                fprintf(f,"%d,%d,%f",i,j, tab[k] );
+                fprintf(f,"(%d.%d),%f\n",i,j, tab[k] );
                 k++;
             }
         }
@@ -29,33 +29,38 @@ void hist_loi_empirique_C9(int n)
 
 void hist_loi_empirique_alter_C10(int n)
 {
+    
     FILE *f = fopen("loi_alter.csv","w"); // ouverture/créée fichier en mode écriture
-    if(f == NULL) // vérifie que le fichier s'eest bien ouvert
+    if(f == NULL) // vérifie que le fichier s'est bien ouvert
     {
         perror("Erreur lors de l'ouverture du fichier");
     }
-    fprintf(f," C^2_1,C^2_2,P_approx_2_alter(n)\n"); // Ligne pour les titres des colonnes
+    
+    fprintf(f,"(C2_1.C2_2),P_approx_2_alter(n)\n"); // Ligne pour les titres des colonnes
     double *tab = P_approx_2_alter(n); //stock les valeurs de P_approx_1(n)
+    printf("test");
+    printf("%f", tab[1]);
     int k =0;
-    while(tab[k]<6)
+    while(k<6)
     {
-        for (int i=0; i<4; i++)
+        
+        for (int i=1; i<4; i++)
         {
             for(int j=i; j<4; j++)
             {
-                fprintf(f,"%d,%d,%f",i,j, tab[k] );
+                fprintf(f,"(%d.%d),%f\n",i,j, tab[k] );
                 k++;
             }
         }
     }
-    fclose(f);
 }
 
 
 int main(){
-    int n = 10;
+    int n = 100;
     hist_loi_empirique_C9(n);
     hist_loi_empirique_alter_C10(n);
+    
     return 0;
 }
 
