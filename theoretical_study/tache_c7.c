bc5b1f6e6c432cacce45c1730286706354492dab
Lilou MALFOY <lilou.malfoy@ensiie.eu>
2025-04-25 14:31:05 +0000

    Replace tache_c7.c

--- theoretical_study/tache_c7.c
+++ theoretical_study/tache_c7.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "tache_c3.h"
-#include "tache_c6.h"
-#include "tache_c5.h"
+#include "tache_c6.c"
+#include "tacheC5.c"
+
+/** 
+\brief :  rempli un csv avec les valeurs de la loi empirique de (C^2_1,C^2_2), conditionnellement à (C^1_1,C^1_2) = (1,2)
+\param : n le nombre de distributions indépendantes
+\return :  nothing
+*/
 
 int hist_empirique_C7(int n)
 {
@@ -12,26 +18,28 @@ int hist_empirique_C7(int n)
         printf("Erreur d'ouverture du fichier\n");
         return 1;
     }
-    // fprintf(file, "freq, (i,j)");
-    // for (int i=1; i<4; i++){
-    //     for (int j=i; j<4; j++){
-    //         fprintf(file, "%f, (%d,%d)", P_approx_2_1(1,2,n), i, j);
-    //     }
-    // }
-
-    double *result = P_approx_2_1(1, 2, n);
+    double *result = P_approx_2_1(1, 2, n); //on stocke les valeurs dans un tableau
     int index = 0;
-    fprintf(file, "P_approx_2_1(1,2,n), (i,j)\n");
+    fprintf(file, "proportion, (i,j)\n");
     for (int i = 1; i < 4; i++)
     {
         for (int j = i; j < 4; j++)
         {
             fprintf(file, "%f, (%d,%d)\n", result[index], i, j);
+            index ++;
         }
     }
     fclose(file);
+    free(result);
     return 0;
 }
+
+
+/** 
+\brief :  rempli un csv avec les valeurs de la loi théorique de (C^2_1,C^2_2), conditionnellement à (C^1_1,C^1_2) = (1,2)
+\param : nothing
+\return :  nothing
+*/
 
 int hist_theorique_C7()
 {
@@ -46,12 +54,19 @@ int hist_theorique_C7()
     {
         for (int j = i; j < 4; j++)
         {
-            fprintf(fd, "%f, (%d, %d)\n", P_cond(i, j, 1, 2), i, j);
+            fprintf(fd, "%f, (%d, %d)\n", P_cond(1,2 , i, j), i, j);
         }
     }
     fclose(fd);
     return 0;
 }
+
+
+/** 
+\brief :  rempli un csv avec les valeurs P_approx_2_1_main(2,3,1,2,n) pour n allant de 1 à 100000
+\param : nothing
+\return :  nothing
+*/
 
 int nuage_C7()
 {
@@ -61,14 +76,20 @@ int nuage_C7()
         printf("Erreur d'ouverture du fichier\n");
         return 1;
     }
-    fprintf(f, "N, P_approx_2_1_main(2,3,1,2,n)\n");
-    for (int i = 1; i < 10001; i++)
+    fprintf(f, "N, proba\n");
+    for (int i = 1; i < 100001; i+= 10) //on crée une boucle de 1 à 100000
     {
         fprintf(f, "%d, %f\n", i, P_approx_2_1_main(2, 3, 1, 2, i));
     }
     fclose(f);
     return 0;
 }
+
+/** 
+\brief :  rempli un csv avec la droite  y = P( (C^2_1,C^2_2) = (2,3) | (C^1_1,C^1_2)=(1,2) )
+\param : nothing
+\return :  nothing
+*/
 
 int droite()
 {
@@ -79,13 +100,14 @@ int droite()
         return 1;
     }
     fprintf(fdd, "y\n");
-    fprintf(fdd, "%f\n", P_cond(2, 3, 1, 2));
+    fprintf(fdd, "%f\n", P_cond(2, 3, 1, 2)); //valeur de P( (C^2_1,C^2_2) = (2,3) | (C^1_1,C^1_2)=(1,2) )
     fclose(fdd);
     return 0;
 }
 
 int main()
 {
+    srand(time(NULL)); //Initialise le générateur de variables aléatoires
     hist_empirique_C7(10000);
     hist_theorique_C7();
     nuage_C7();
