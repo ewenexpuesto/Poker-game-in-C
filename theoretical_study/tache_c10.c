de6f78ce19462a3a2fd459ca0de1c5d6a3eeab7a
Sylvain CHAU <sylvain.chau@ensiie.eu>
2025-04-25 19:06:09 +0200

    tache C10

--- theoretical_study/tache_c10.c
+++ theoretical_study/tache_c10.c
@@ -9,10 +9,10 @@
  * Retourne la proportion des mains obtenues par J2
  */
 
-double P_approx_2_1_alter(int i_prim, int j_prim, int n){
+double P_approx_2_main_alter(int i_prim, int j_prim, int n){
     double P_B = 0;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
+    for(int i = 1; i < 4; i++){
+        for(int j = 1; j < 4; j++){
             P_B += P_approx_1_main(i,j,n) * P_approx_2_1_main(i_prim,j_prim,i,j,n);
         }
     }
@@ -23,14 +23,21 @@ double* P_approx_2_alter(int n){
     double* tab = malloc(6 * sizeof(double));
     double P_B = 0;
     int count = 0;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            P_B = P_approx_2_1(i,j,n)[count];
-            tab[count] = P_B;
-            count++;
+    for(int k = 0; k < 6;k++ ){
+        for(int i = 1; i < 4; i++){
+            for(int j = i; j < 4; j++){
+                P_B += P_approx_2_1(i,j,n)[count] ;
+                //printf(" sachant : %f\n", P_approx_2_1(i,j,n)[count]);
+            }
         }
+        printf("p_approx %f \n : ",P_approx_1(n)[count]);
+        tab[count] = P_B * P_approx_1(n)[count];
+        //printf(" tab : %f\n", tab[count]);
+        P_B = 0;
+        count++;
     }
 
+    printf(" somme :%f \n", P_approx_1(n)[0] + P_approx_1(n)[1] + P_approx_1(n)[2] + P_approx_1(n)[3] + P_approx_1(n)[4] + P_approx_1(n)[5]);
     return tab;
 }
 
